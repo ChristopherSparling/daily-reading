fc685876d47bc28aee6937f6f5b9d6159b776292
ChristopherSparling <christopher.j.sparling@gmail.com>
2019-10-05 09:44:18 -0400

    Formatted text message body
    Also modifed csv header 'rank' to 'card_rank' to eliminate conflict with DataFrame method

--- daily-reading.py
+++ daily-reading.py
@@ -3,14 +3,6 @@ import random as rnd
 import pandas as pd
 from hidden import credentials
 
-# email_text = """
-# Subject: %s
-# Daily Readings: 
-# %s
-# %s
-# """ % (subject, "\n ".join(), )
-
-
 def send(message):
     to_number = credentials['number'] + credentials['carrier']
     auth = (credentials['user'], credentials['password'])
@@ -26,11 +18,12 @@ def send(message):
 def getBookList(books_csv):
     """
     books_csv keys:
-    []'suit', 'rank', 'title', 'author', 'recommended_divisions',
+    ['suit', 'rank', 'title', 'author', 'recommended_divisions',
        'division_word', 'completed']
     """
+
     bookList = pd.DataFrame(
-        columns=['suit', 'rank', 'title', 'author', 'recommended_divisions',
+        columns=['suit', 'card_rank', 'title', 'author', 'recommended_divisions',
        'division_word', 'completed']
        )
     books_df = pd.read_csv(books_csv, sep=',', header=0)
@@ -39,9 +32,31 @@ def getBookList(books_csv):
     for suit, groupdata in incomplete_books_df.groupby('suit', as_index=False):
         randint = rnd.randrange(len(groupdata))
         bookList = pd.concat([bookList,groupdata.iloc[[randint]]], ignore_index=True)
-
+        # print(bookList)
     return bookList
         
+def createMessage (bookList) :
+    # """
+    # readings format:
+    #     book.title \n
+    #     book.author \n
+    #     book.suit|book.rank book.reccomended_divisions book.division_word \n
+    #     \n
+    # """
+    # print('BookList:\n',bookList)
+    # for index,book in bookList.iterrows():
+    #     print(index, book.card_rank)
+    # print( [book.rank for index, book in bookList.iterrows()]
+    # )
+    readings = "\n\n".join(
+        [f"{book.title}\n{book.author}\n{book.suit}{book.card_rank}|{book.recommended_divisions} {book.division_word}" for index, book in bookList.iterrows()]
+        )
+    email_text = f"""
+    Subject: Daily Readings
+    {readings}
+     """
+    print(email_text)
 
 if __name__ == "__main__":
-    print(getBookList('books.csv'))
+    bookList = getBookList('books.csv')
+    createMessage(bookList)
